6dcb0d0029f7dadf79dc80144ebe2d18bbba699e
chuongdk <chuongdk@gmail.com>
2017-06-28 14:55:06 +0200

    update script for the cluster

--- runWebdocs.sh
+++ runWebdocs.sh
@@ -6,15 +6,16 @@ echo "Running .................................................."
 
 
 
-input="input/webdocsX100.dat"
+input="input/webdocs.dat"
 #zeroOne = 0.1%
-zeroOne=169208
+#zeroOne=169208
+zeroOne=1692
 
 memory=4000
 maxDatabase=150
+nReducers=30
 
-
-for i in 200 150 100 90
+for i in 100 
 do 
 	echo "I=$i"
 	support=$((  $zeroOne * i  )) 
@@ -23,7 +24,8 @@ do
 
 	echo "hadoop jar target/MApriori-0.0.1-SNAPSHOT.jar  girafon.MApriori.App $input output $support $memory $maxDatabase"
 
-	hadoop jar target/MApriori-0.0.1-SNAPSHOT.jar  girafon.MApriori.App $input output $support $memory $maxDatabase
+	hadoop jar target/MApriori-0.0.1-SNAPSHOT.jar  girafon.MApriori.App $input output $support $memory $maxDatabase $nReducers /home/prof/chuong/workspace/eclat/src/eclat 1000000
+
 done
 
 
