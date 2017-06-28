7580e95a1b0bf99858e1a830d4f61477b39f7985
chuongdk <chuongdk@gmail.com>
2017-06-28 16:37:03 +0200

    update .sh

--- runKosarak.sh
+++ runKosarak.sh
@@ -5,27 +5,26 @@ mvn package
 echo "Running .................................................."
 
 
-
 input="input/kosarakX1k.dat"
 #zeroOne = 0.1%
 zeroOne=990002
-#zeroOne=990
-memory=2000
+
+memory=4000
 maxDatabase=150
+nReducers=30
+trieSize=10000000
 
+for i in 10 8 6
+do
+        echo "I=$i"
+        support=$((  $zeroOne * i  ))
+        echo "MapFIM   support = $support, memory = $memory,  input = $input,  max number of Database=$maxDatabase"
+        hdfs dfs -rm -r /user/chuong/output
 
-for i in 10 5 3 1
-do 
-	echo "I=$i"
-	support=$((  $zeroOne * i  )) 
-	echo "MapFIM   support = $support, memory = $memory,  input = $input,  max number of Database=$maxDatabase"
-	hdfs dfs -rm -r /user/chuong/output
+        echo "hadoop jar target/MApriori-0.0.1-SNAPSHOT.jar  girafon.MApriori.App $input output $support $memory $maxDatabase"
 
-	echo "hadoop jar target/MApriori-0.0.1-SNAPSHOT.jar  girafon.MApriori.App $input output $support $memory $maxDatabase"
+        hadoop jar target/MApriori-0.0.1-SNAPSHOT.jar  girafon.MApriori.App $input output $support $memory $maxDatabase $nReducers /home/prof/chuong/workspace/eclat/src/eclat $trieSize
 
-	hadoop jar target/MApriori-0.0.1-SNAPSHOT.jar  girafon.MApriori.App $input output $support $memory $maxDatabase
 done
-
-
 
 
